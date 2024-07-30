c1c70d0936524c3f1290be32d51bfd9406e6d6da
matheus pinto <mat.vsp@gmail.com>
2024-07-30 11:20:31 -0300

    Working with GPU

--- c++/gpu.cpp
+++ c++/gpu.cpp
@@ -6,8 +6,8 @@
 #include <thread>
 #include <future>
 
-float dt = 0.1f;
-int numParticles = 1000;
+float dt = 0.5f;
+int numParticles = 300000;
 
 class particle{
 public:
@@ -20,21 +20,24 @@ public:
     particle(sf::Vector2f position_, sf::Vector2f velocities_)
         : position(position_), velocities(velocities_), pixel(position,sf::Color::White){
     };
-    void update(sf::RenderWindow& window){
-        sf::Vector2i mousePos = sf::Mouse::getPosition(window);
+    void update(sf::Vector2i mousePos){
         acceleration = sf::Vector2f(mousePos) - position;
         float length = std::sqrt(acceleration.x*acceleration.x + acceleration.y*acceleration.y);
         acceleration /= length;
         velocities = velocities + acceleration*(dt * 10);
+        velocities.x *= 0.99;
+        velocities.y *= 0.99;
         position += velocities * dt;
         pixel.position = position;
     }
 };
 
-void update(int initial, int final, std::vector<particle>& particles, sf::VertexArray& vertices, sf::RenderWindow& window){
+void update(int initial, int final, std::vector<particle>& particles, sf::VertexArray& vertices, sf::Vector2i mousePos){
     for(int i  = initial; i < final; i++){
-        particles[i].update(window);
+        particles[i].update(mousePos);
         vertices[i].position = particles[i].position;
+        //int x = i % 255;
+        //vertices[i].color = sf::Color(x+ 30,x+10,x+50);
     }
 }
 
@@ -42,6 +45,7 @@ void update(int initial, int final, std::vector<particle>& particles, sf::Vertex
 int main(){
     sf::VideoMode desktop = sf::VideoMode::getDesktopMode();
     sf::RenderWindow window(desktop, "particles", sf::Style::Fullscreen);
+    window.setFramerateLimit(120);
     sf::Clock clock;
     std::random_device rd;
     std::mt19937 gen(rd());
@@ -53,7 +57,7 @@ int main(){
         particle p(sf::Vector2f(value(gen),value(gen)),sf::Vector2f(0,0));
         particles.emplace_back(p);
         vertices[i].position = p.position;
-        vertices[i].color = sf::Color::White;
+        vertices[i].color = sf::Color(233, 128, 110);
     }
     while(window.isOpen()){
 
@@ -65,6 +69,7 @@ int main(){
             }
         }
         window.clear(sf::Color::Black);
+        sf::Vector2i mousePos = sf::Mouse::getPosition(window);
 
         const int numThreads = std::thread::hardware_concurrency();
         std::vector<std::future<void>> futures;
@@ -73,7 +78,7 @@ int main(){
         for(int t = 0; t < numThreads; ++t){
             int start = t * chunkSize;
             int end = (t == numThreads - 1) ? numParticles : start + chunkSize;
-            futures.emplace_back(std::async(std::launch::async,update, start, end, std::ref(particles),std::ref(vertices), std::ref(window)));
+            futures.emplace_back(std::async(std::launch::async,update, start, end, std::ref(particles),std::ref(vertices), mousePos));
         }
 
         for(auto& future : futures){
